af6159f1366085e6f994b11d343dbc4d816f62c2
Anand Mallik <ccanandmallik@gmail.com>
2022-06-17 00:02:43 -0400

    adds tests on camera movement

--- Scenes/camera_gimbal/test_camera_gimbal.gd
+++ Scenes/camera_gimbal/test_camera_gimbal.gd
@@ -30,9 +30,57 @@ class Test__physics_process:
 
 	func test__move_forward():
 		var init_position_z = double_script.get_translation().z
-
 		_input.press("move_forward")
-
 		gut.simulate(double_script, 200, 1)
-
 		assert_lt(double_script.get_translation().z, init_position_z, "Can move forward")
+
+
+	func test__move_backward():
+		var init_position_z = double_script.get_translation().z
+		_input.press("move_backward")
+		gut.simulate(double_script, 200, 1)
+		assert_gt(double_script.get_translation().z, init_position_z, "Can move backward")
+
+
+	func test__move_left():
+		var init_position_x = double_script.get_translation().x
+		_input.press("move_left")
+		gut.simulate(double_script, 200, 1)
+		assert_lt(double_script.get_translation().x, init_position_x, "Can move left")
+
+
+	func test__move_right():
+		var init_position_x = double_script.get_translation().x
+		_input.press("move_right")
+		gut.simulate(double_script, 200, 1)
+		assert_gt(double_script.get_translation().x, init_position_x, "Can move right")
+
+
+	func test__move_down():
+		var init_position_y = double_script.get_translation().y
+		_input.press("move_down")
+		gut.simulate(double_script, 200, 1)
+		assert_lt(double_script.get_translation().y, init_position_y, "Can move down")
+
+
+	func test__move_up():
+		var init_position_y = double_script.get_translation().y
+		_input.press("move_up")
+		gut.simulate(double_script, 200, 1)
+		assert_gt(double_script.get_translation().y, init_position_y, "Can move up")
+
+
+	func test__rotate_left():
+		var init_rotate = double_script.get_rotation_degrees().y
+		_input.press("rotate_left")
+		gut.simulate(double_script, 200, 1)
+		assert_lt(double_script.get_rotation_degrees().y, init_rotate, "Can rotate left")
+
+
+	func test__rotate_right():
+		var init_rotate = double_script.get_rotation_degrees().y
+		_input.press("rotate_right")
+		gut.simulate(double_script, 200, 1)
+		assert_gt(double_script.get_rotation_degrees().y, init_rotate, "Can rotate right")
+
+
