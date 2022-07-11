b6e5dcbc2823ecae6769b01ff588b426dcd019e9
Anand Mallik <ccanandmallik@gmail.com>
2022-07-10 20:17:57 -0400

    adds a vertical pivot to the camera

--- Scenes/camera_gimbal/camera_gimbal.gd
+++ Scenes/camera_gimbal/camera_gimbal.gd
@@ -1,9 +1,10 @@
 extends Spatial
 
 export(float) var camera_gimbal_speed = 0.135
+export(float) var inner_gimbal_rotate_speed = 0.0035
 export(float) var rotate_speed = 0.035
 export(float) var camera_gimbal_floor_change_speed = 0.2
-export(float) var camera_zoom_speed = 0.16
+export(float) var camera_zoom_speed = 0.3
 export(int) var camera_y_diff_per_floor = 1
 export(int) var camera_zoom_tick = 1
 
@@ -17,6 +18,8 @@ onready var target_y: int = self.get_translation().y
 onready var y_offset: int = self.get_translation().y
 onready var camera = find_node("Camera")
 onready var camera_transform = camera.get_transform()
+onready var inner_gimbal = find_node("inner_gimbal")
+onready var inner_gimbal_transform = inner_gimbal.get_transform()
 onready var target_zoom = camera.get_translation().z
 
 # Pass a mock input object for testing
@@ -28,12 +31,15 @@ func change_floor(target_floor):
 	target_y = (target_floor - 1 * camera_y_diff_per_floor) + y_offset
 
 
-func _unhandled_input(_event):
-	if inputter.is_action_just_released("zoom_in"):
+func _unhandled_input(event):
+	if event.is_action_just_released("zoom_in"):
 		target_zoom -= camera_zoom_tick
 
-	if inputter.is_action_just_released("zoom_out"):
+	if event.is_action_just_released("zoom_out"):
 		target_zoom += camera_zoom_tick
+
+	if event is InputEventMouseMotion and event.is_action_pressed("third_button"):
+		inner_gimbal.rotate_x(PI * event.relative.y * inner_gimbal_rotate_speed)
 
 
 func _physics_process(_delta):
@@ -45,7 +51,7 @@ func _physics_process_camera():
 	var zdiff = abs(abs(camera_transform.origin.z) - abs(target_zoom))
 
 	if (camera_transform.origin.z != target_zoom):
-		if (zdiff < .1):
+		if (zdiff < camera_zoom_speed):
 			camera_transform.origin.z = target_zoom
 		else:
 			if (camera_transform.origin.z < target_zoom):
