1c9f520b4199ad9e7beedeca66a134dd0a2bea91
Anand "New Noise" Mallik <amallik@newnoiseworks.com>
2023-11-21 21:14:32 -0500

    reduces warnings

--- Scenes/floor/test_room_manager.gd
+++ Scenes/floor/test_room_manager.gd
@@ -14,7 +14,7 @@ class Test__can_place_room_at:
 
 		room_manager = prototype_script.new()
 		room_manager_node = Node3D.new()
-		room_manager_node.set_script(room_manager)
+		# room_manager_node.set_script(room_manager)
 
 		var data = {
 			0: {
@@ -75,7 +75,7 @@ class Test__can_place_room_at:
 		var result = room_manager._can_place_room_at(Vector3(0, 0, 0))
 
 		assert_true(result, "Can place room after rotating four times")
-		# assert_true(room_manager.floor_data_details)
+		assert_true(room_manager.floor_data_details != null)
 
 
 class Test__rotate_hover_item:
@@ -90,7 +90,7 @@ class Test__rotate_hover_item:
 
 		room_manager = prototype_script.new()
 		room_manager_node = Node3D.new()
-		room_manager_node.set_script(room_manager)
+		# room_manager_node.set_script(room_manager)
 
 		var data = {
 			0: {
