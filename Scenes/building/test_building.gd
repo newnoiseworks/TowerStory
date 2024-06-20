6c4f57f79d262f708ba664e2b59dc68f081c5221
Anand "New Noise" Mallik <amallik@newnoiseworks.com>
2024-06-19 23:55:37 -0400

    failing test to check for double hover item issue

--- Scenes/building/test_building.gd
+++ Scenes/building/test_building.gd
@@ -447,6 +447,7 @@ class Test_SecondFloorWorkflow:
 		test_building._set_input(input)
 		add_child_autofree(test_building)
 
+
 	func test_add_piece_where_one_exists():
 		# first, make a piece on the first floor
 		input._click_and_release(
@@ -753,5 +754,59 @@ class Test_AddRoomWorkflow:
 		var office_corner_piece = test_building.get_node("floors/floor1/office_corner/tiles/bottomFloorPiece")
 
 		assert_false(office_corner_piece.is_transparent)
+
+
+	func test_can_add_room_to_second_floor_without_creating_hover_item_on_first_floor():
+		input.press("move_up")
+		test_building._unhandled_input(input)
+		gut.simulate(test_building, 2, 20)
+		input.release("move_up")
+		test_building._unhandled_input(input)
+		gut.simulate(test_building, 2, 20)
+
+		input._click_and_drag_and_release(
+			test_building,
+			Vector3(
+				0.076785, 0.100007, 0.179358
+			),
+			Vector3(
+				2.076785, 0.100007, 4.179358
+			)
+		)
+		gut.simulate(test_building, 20, 200)
+
+		var room_manager_2 = test_building.get_node_or_null("floors/floor2/floor/room_manager")
+
+		assert_not_null(room_manager_2, "Second floor room manager null when it should exist")
+
+		TowerGlobals.tool_change.emit(TowerGlobals.UI_TOOL.SMALL_OFFICE_CORNER)
+		gut.simulate(test_building, 2, 2)
+
+		input._move(
+			test_building,
+			Vector3.ZERO
+		)
+		gut.simulate(test_building, 2, 2)
+
+		input.input_method = "_on_button_click"
+
+		input._click_and_release(
+			test_building,
+			Vector3.ZERO
+		)
+		gut.simulate(test_building, 2, 2)
+
+		input._reset()
+
+		assert_null(room_manager_2._hover_item, "Second floor still has hover item when it shouldn't")
+
+		input.press("move_down")
+		test_building._unhandled_input(input)
+		gut.simulate(test_building, 2, 20)
+		input.release("move_down")
+		test_building._unhandled_input(input)
+		gut.simulate(test_building, 2, 20)
+
+		assert_null(room_manager._hover_item, "First floor still has hover item when it shouldn't have in the first place")
 
 
